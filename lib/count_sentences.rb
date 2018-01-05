66447ca39a170493a5ab7b5755b6e89b320cf6c5
Onur Eker <yonureker@gmail.com>
2018-01-05 20:03:57 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split[/.?!/].filter{|x| x != ""}.count
+    a.split[/.?!/].filter{|x| x != ""}.count
   end
 end
